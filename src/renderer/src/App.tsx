39408b57f43a3380c9823083551c22c3f15cc2ee
Fernando Ortiz <fernandodev.ortiz@gmail.com>
2025-09-04 16:40:03 -0600

    Todo: Configure router and tailwindcss

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,11 @@
+function App(): React.JSX.Element {
+  // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+    </>
+  )
+}
+
+export default App
